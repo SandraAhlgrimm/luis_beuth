cab1eb0774f7e18dbc4b93d65841af6428107f54
Kriemann <skrieman@XIN851734.asv.local>
2016-10-21 22:45:29 +0200

    Rest Student Service

---
+++ Controllers/ApiStudentController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using luis_beuth.Models;
+using luis_beuth.Models.Data;
+using luis_beuth.Services;
+using luis_beuth.Data;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace luis_beuth.Controllers
+{
+    [Route("api/student")]
+    public class ApiStudentController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ApiStudentController (ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // 
+        // GET: /Student/
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return _context.Student.ToList();
+        }
+
+        // 
+        // GET: /Student/{Id}/ 
+        [HttpGet]
+        public Student Get(int id)
+        {
+            return _context.Student.FirstOrDefault(p => p.Id == id);
+        }
+
+        // 
+        // POST: /Student
+    }
+}
--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -3,14 +3,21 @@ using System.Text.Encodings.Web;
 using luis_beuth.Models;
 using luis_beuth.Models.Data;
 using luis_beuth.Services;
+using luis_beuth.Data;
 
 namespace luis_beuth.Controllers
 {
     public class StudentController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public StudentController (ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
         // 
         // GET: /Student/
-
         public string Index()
         {
             return "This is my Student action...";

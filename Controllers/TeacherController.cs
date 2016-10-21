070b454e5ad35b1968d8f78149026aa78ad34534
Marcel Malik <marcel@einmalik.eu>
2016-10-21 22:03:34 +0200

    add teacher list and edit operations

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,21 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using luis_beuth.Models;
 using luis_beuth.Models.Data;
 using luis_beuth.Services;
+using luis_beuth.Data;
 
 namespace luis_beuth.Controllers
 {
     public class TeacherController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext _context;
+        public TeacherController (ApplicationDbContext context)
         {
-            return View();
+            this._context = context;
         }
 
-        public IActionResult GetById(int id)
+        // GET: Teachers
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await this._context.Teacher.ToListAsync());
+        }
+
+        // GET: Teacher/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await this._context.Teacher.SingleAsync(p => p.Id == id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+            return View(teacher);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Teacher teacher)
+        {
+            if (id != teacher.Id)
+            {
+                return NotFound();
+            }
+            
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    this._context.Update(teacher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!this.TeacherExists(teacher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return this._context.Teacher.Any(p => p.Id == id);
         }
     }
 }

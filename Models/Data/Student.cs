81d7c45e613f8c508dda8f0523d30a5334c7d8cd
Marcel Malik <marcel@einmalik.eu>
2016-10-22 01:26:40 +0200

    - add id fields to edit views - update translation

--- Models/Data/Student.cs
+++ Models/Data/Student.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace luis_beuth.Models.Data
 {
     public class Student 
     {
         public int Id { get; set; }
-        public int MatriculationNumber { get; set;}
+
+        [Display(Name = "Matrikelnummer")]
+        public int MatriculationNumber { get; set; }
+
+        [Display(Name = "Zugelassen")]
         public bool Approved { get; set; }
         public List<Rent> Rents { get; }
         public string Name { get; set; }

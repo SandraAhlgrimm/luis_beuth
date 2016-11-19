e0204a67ada88032ae15bf19c3453850c23efe08
SandraKriemann <sandra.kriemann@gmail.com>
2016-11-19 14:32:26 +0100

    FSRLogin

---
+++ Controllers/ApiLoginController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using luis_beuth.Data;
+using luis_beuth.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace luis_beuth.Controllers
+{
+
+    [Route("api/login")]
+    public class ApiLoginController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ApiLoginController (ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        //
+        // GET: /api/login?{Matriculationnumber}&{PasswordHash}/
+        [HttpGet("{matriculationNumber}")]
+        public IActionResult Get(int matriculationNumber, string passwordHash)
+        {
+            if (_context.Logins.FirstOrDefault(l => l.PasswordHash == passwordHash) != null)
+                return NoContent();
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Models/Data/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace luis_beuth.Models.Data
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string PasswordHash { get; set;}
+        public DateTime Created { get; set; }
+    }
+}
